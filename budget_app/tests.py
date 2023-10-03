e33464dc545d126b6dd54d917d4e89ec4ef002af
Artyom Vetlugin <artyom.vetlugin@gmail.com>
2023-10-03 22:17:39 +0600

    added one autotest to check

--- budget_app/tests.py
+++ budget_app/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .forms import TransactionForm
 
-# Create your tests here.
+class TransactionFormTest(TestCase):
+    
+    def test_clean_amount(self):
+        form = TransactionForm({'amount': -10, 'transaction_type': 'E', 'description': 'test', 'date': '2022-01-01'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['amount'], ['Сумма должна быть положительной'])
+
